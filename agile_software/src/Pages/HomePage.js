e67e0f3e3273d0f2f4428598c62b3ad21a532060
CeciliaN99 <cecnyb@net.chalmers.se>
2023-04-11 15:56:01 +0200

    FrontPage opens

---
+++ agile_software/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My Page</h1>
+      <p>This is my first React page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
