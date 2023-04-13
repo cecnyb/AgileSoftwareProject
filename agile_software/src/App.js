9dd3a9addb11264ae98cb6d2ab2f2c16a3898d33
jonasrost <rojonas@student.chalmers.se>
2023-04-13 11:34:52 +0200

    Added login page

--- agile_software/src/App.js
+++ agile_software/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import PageWithText from "./Pages/PageWithText";
+import Login from "./Pages/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/PageWithText" element={<PageWithText />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
