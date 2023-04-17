1695ed7f4552e965e65ae1bbb2cb55923f839201
Olivert0187 <oliver.brink@hotmail.se>
2023-04-17 14:52:37 +0200

    Update index.js

--- agile_software/src/index.js
+++ agile_software/src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+import ReactDOM from "react-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="login" element={<Login />} />
+            <Route path="profile" element={<Profile />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
+
 
 
 // If you want to start measuring performance in your app, pass a function
