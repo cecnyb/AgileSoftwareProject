d5560cb2fc181464a76d493942dd1f17442ccc87
jonasrost <73895167+jonasrost@users.noreply.github.com>
2023-04-17 14:41:45 +0200

    Add functions for sign in, sign up and sign up to firebase.js

--- agile_software/src/firebase.js
+++ agile_software/src/firebase.js
@@ -29,3 +29,49 @@ import {
   import { getFirestore, addDoc, collection } from "firebase/firestore";
   const db = getFirestore();
   const auth = getAuth();
+
+// Sign up
+const signUp = async (email, password) => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        email: user.email,
+      });
+      return true
+    } catch (error) {
+      return {error: error.message}
+    }
+  };
+
+  // Sign in
+  const signIn = async (email, password) => {
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        const user = userCredential.user;
+        return true
+      } catch (error) {
+        return {error: error.message}
+      }
+    };
+
+// Sign out
+const signOut = async() => {
+    try {
+      await signOut(auth)
+      return true
+    } catch (error) {
+      return false
+    }
+  };
+
+
