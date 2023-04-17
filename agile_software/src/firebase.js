58fe2a99b55579214517135bd9aeb1070408ab70
jonasrost <73895167+jonasrost@users.noreply.github.com>
2023-04-17 14:20:48 +0200

    Create firebase.js

---
+++ agile_software/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMg7qvLxlpbnM1ayFnep7Y_xShenT8EkQ",
+  authDomain: "vk-vattenskoter.firebaseapp.com",
+  projectId: "vk-vattenskoter",
+  storageBucket: "vk-vattenskoter.appspot.com",
+  messagingSenderId: "967558583555",
+  appId: "1:967558583555:web:be81fea2b86c02be494536",
+  measurementId: "G-CP25S8F63K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
