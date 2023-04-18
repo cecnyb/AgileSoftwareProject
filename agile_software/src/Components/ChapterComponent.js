852ba44e0f804b05f43d8c163ffb46fc932f825f
wilhelmj <wilhelmj@chalmers.se>
2023-04-18 14:10:01 +0200

    Added documentation for the ChapterComponent.js

--- agile_software/src/Components/ChapterComponent.js
+++ agile_software/src/Components/ChapterComponent.js
@@ -1,6 +1,13 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
 
+/**
+ * A component with a link to a corresponding chapter
+ * @author Wilhelm, Lukas & Cecilia
+ * @param {object} props - A chapter object from chapters.js
+ * @return {JSX} - A JSX element with a link to the chapter
+ */
+
 function ChapterComponent(props) {
     return (
         <section key={props.chapter.id} className="summary">
