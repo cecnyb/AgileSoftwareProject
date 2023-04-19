5a5fe44353543931c763b4e528a31ffb7c2af68d
wilhelmj <wilhelmj@chalmers.se>
2023-04-19 10:21:43 +0200

    La till kapitel samt underkaptitel i chapters.js

--- agile_software/src/chapters.js
+++ agile_software/src/chapters.js
@@ -11,9 +11,33 @@ const chapters = [
                         ,text: 'This course is designed to teach you how to use a Jet Ski. Jet skiing is a popular recreational activity that involves riding on a personal watercraft, also known as a jet ski. Jet skis are small, motorized boats that are propelled by a jet of water that is created by a pump and an impeller. Jet skis are typically operated by a single rider, but some models can accommodate two or more passengers. Jet skis are often used for recreational purposes, such as water sports, sightseeing, and fishing. They can also be used for commercial purposes, such as search and rescue operations, law enforcement, and military operations.'
                     }
                 ]
+            },
+            {
+                id: 2,
+                title: 'Förtöjning',
+                content: [
+                    {header: 'Förtöjning - exempeltext'
+                        ,text: 'This course is designed to teach you how to use a Jet Ski. Jet skiing is a popular recreational activity that involves riding on a personal watercraft, also known as a jet ski. Jet skis are small, motorized boats that are propelled by a jet of water that is created by a pump and an impeller. Jet skis are typically operated by a single rider, but some models can accommodate two or more passengers. Jet skis are often used for recreational purposes, such as water sports, sightseeing, and fishing. They can also be used for commercial purposes, such as search and rescue operations, law enforcement, and military operations.'
+                    }
+                ]
             }
         ]
     },
+    {
+        id: 2, 
+        title: 'Sjökunskap',
+        subchapters: [
+            {
+                id: 1,
+                title: 'De internationella sjövägsreglerna - vad är det?',
+                content: [
+                    {header: 'Vad är de internationella sjövägsreglerna?'
+                        ,text: 'This course is designed to teach you how to use a Jet Ski. Jet skiing is a popular recreational activity that involves riding on a personal watercraft, also known as a jet ski. Jet skis are small, motorized boats that are propelled by a jet of water that is created by a pump and an impeller. Jet skis are typically operated by a single rider, but some models can accommodate two or more passengers. Jet skis are often used for recreational purposes, such as water sports, sightseeing, and fishing. They can also be used for commercial purposes, such as search and rescue operations, law enforcement, and military operations.'
+                    }
+                ]
+            }
+        ]
+    }
 ]
 
 export default chapters;
