50374d08c4ae509cb96e1fd5bc6ea1cf157241cf
jonasrost <73895167+jonasrost@users.noreply.github.com>
2023-04-25 13:21:22 +0200

    Fixed bug that led to users not being logged in
    Also changed language on page to Swedish

--- agile_software/src/index.js
+++ agile_software/src/index.js
@@ -7,8 +7,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Signin";
 import Signup from './Pages/Signup';
 import Profile from "./Profile";
-import App from './App';
-import { AuthProvider } from "./AuthProvider";
+import HomePage from "./Pages/HomePage";
+import Subchapter from "./Pages/Subchapter";
+import Chapter from "./Pages/Chapter";
+import { AuthProvider } from "./AuthProvider"; //Has to be the last import statement, else it doesn't work
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 //root.render(
@@ -20,6 +22,9 @@ ReactDOM.render(
             <Route path="/" element={<Login />} />
             <Route path="signup" element={<Signup />} />
             <Route path="profile" element={<Profile />} />
+            <Route path="/HomePage" element={<HomePage />} />
+            <Route path="/chapter/:chapterId" element={<Chapter/>}/>
+            <Route path="/chapter/:chapterId/subchapter/:subchapterId" element={<Subchapter/>}/>
           </Routes>
         </AuthProvider>
       </BrowserRouter>
