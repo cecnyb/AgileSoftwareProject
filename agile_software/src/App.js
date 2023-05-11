4de51fb3d969affbcca8f0787193d6aeca21e5cf
luced01 <lucas.edeslatt@hotmail.se>
2023-05-11 09:33:36 +0200

    Bugfixes

--- agile_software/src/App.js
+++ agile_software/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Login from "./Pages/Signin";
@@ -7,18 +6,11 @@ import Profile from "./Profile";
 import Subchapter from "./Pages/Subchapter";
 import Chapter from "./Pages/Chapter";
 import DatabaseAccess from "./Pages/DatabaseAccess";
-=======
-import logo from './logo.svg';
-import './App.css';
-import Homepage from "./Pages/HomePage";
-import PageWithText from "./Pages/PageWithText";
 
->>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
-<<<<<<< Updated upstream
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/HomePage" element={<HomePage />} />
@@ -27,26 +19,7 @@ function App() {
         <Route path="/DatabaseAccess" element={<DatabaseAccess />} />
       </Routes>
     </div>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    <div className="App-content">
-    <Homepage />
-    </div> 
-  </div>
->>>>>>> Stashed changes
+
   );
 }
 
--- agile_software/src/Pages/HomePage.js
+++ agile_software/src/Pages/HomePage.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< Updated upstream
 import chapters from "../chapters";
 import ChapterComponent from "../Components/ChapterComponent";
 
@@ -15,19 +14,4 @@ function HomePage () {
 };
 
 export default HomePage;
-=======
-import { Link } from "react-router-dom";
 
-const Homepage = () => {
-return (
-    <div>
-      <div className="header">
-      <button>Testbutton</button>
-          {/* <Link to="./PageWithText" className="link settingsBtn"></Link> */}
-          </div>  
-    </div>
-  );
-}
-
-export default Homepage;
->>>>>>> Stashed changes
