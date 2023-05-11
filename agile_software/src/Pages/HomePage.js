dc5a07b415ff33b92e3af84a6feaaaeb7686f559
forsellm <forsellm@chalmers.se>
2023-05-11 11:56:04 +0200

    Changed so that if you try to access homepage

--- agile_software/src/Pages/HomePage.js
+++ agile_software/src/Pages/HomePage.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import chapters from "../chapters";
 import ChapterComponent from "../Components/ChapterComponent";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
-import firebase from "firebase/compat/app";
-import { getFirestore, addDoc, collection } from "firebase/firestore";
 
-const db = firebase.firestore();
-const auth = getAuth();
-const user = auth.currentUser;
-
-function HomePage () {
+function HomePage() {
+  const auth = getAuth();
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        navigate('/Signup');
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth, navigate]);
+
   if (user) {
     return (
       <div className="text-center pt-5">
         <header>
-         {db.collection("/users/I3IjH1qzyrY2JUAoW9dM/role").doc.item}
           Welcome to your education {user.email}
         </header>
-      {chapters.map((chapter) => (
+        {chapters.map((chapter) => (
           <ChapterComponent chapter={chapter} key={chapter.id} />
         ))}
-    </div>
+      </div>
     );
   } else {
-    navigate('/');
+    return null; // or you can render a loading indicator
   }
-};
+}
 
 export default HomePage;
