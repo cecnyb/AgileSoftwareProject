9672f009c0565a1697e934dcf89bf731500d2d62
forsellm <forsellm@chalmers.se>
2023-05-11 12:09:29 +0200

    made it wokr better

--- agile_software/src/Pages/HomePage.js
+++ agile_software/src/Pages/HomePage.js
@@ -3,38 +3,20 @@ import chapters from "../chapters";
 import ChapterComponent from "../Components/ChapterComponent";
 import { getAuth } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+import useRequireAuth from '../AuthenticateUser';
 
 function HomePage() {
-  const auth = getAuth();
-  const navigate = useNavigate();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        navigate('/Signup');
-      }
-    });
-
-    return () => unsubscribe();
-  }, [auth, navigate]);
-
-  if (user) {
+  const currentUser = useRequireAuth();
     return (
       <div className="text-center pt-5">
         <header>
-          Welcome to your education {user.email}
+          Welcome to your education {currentUser.email}
         </header>
         {chapters.map((chapter) => (
           <ChapterComponent chapter={chapter} key={chapter.id} />
         ))}
       </div>
     );
-  } else {
-    return null; // or you can render a loading indicator
-  }
 }
 
 export default HomePage;
