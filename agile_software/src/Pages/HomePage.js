b8a909c6a3b637428f655bee63bdb7716888c29c
forsellm <forsellm@chalmers.se>
2023-05-11 12:30:58 +0200

    added logout button to homepage

--- agile_software/src/Pages/HomePage.js
+++ agile_software/src/Pages/HomePage.js
@@ -1,22 +1,33 @@
-import React, { useEffect, useState } from "react";
 import chapters from "../chapters";
 import ChapterComponent from "../Components/ChapterComponent";
-import { getAuth } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
 import useRequireAuth from '../AuthenticateUser';
+import { signout } from '../firebase';
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  signout();
+};
 
 function HomePage() {
   const currentUser = useRequireAuth();
-    return (
-      <div className="text-center pt-5">
-        <header>
-          Welcome to your education {currentUser.email}
-        </header>
-        {chapters.map((chapter) => (
-          <ChapterComponent chapter={chapter} key={chapter.id} />
-        ))}
-      </div>
-    );
+  var userEmail = "";
+  if(currentUser){
+    userEmail=currentUser.email;
+  }
+
+  return (
+    <div className="text-center pt-5">
+      <header>
+        Welcome to your education {userEmail}
+      </header>
+      {chapters.map((chapter) => (
+        <ChapterComponent chapter={chapter} key={chapter.id} />
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input type="submit" value="Logga ut" />
+      </form>
+    </div>
+  );
 }
 
 export default HomePage;
